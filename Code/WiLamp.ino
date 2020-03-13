d00360c68ea0a4f74027ba3fb5846b7e72d9f5d1
Muhammed Fasil K <mhdfasilwyd@gmail.com>
2020-03-13 13:56:58 +0530

    Update WiLamp.ino

--- Code/WiLamp.ino
+++ Code/WiLamp.ino
@@ -24,23 +24,29 @@ IPAddress gateway(192, 168, 43, 1);
 IPAddress subnet(255, 255, 255, 0);  
 IPAddress dns(8, 8, 8, 8);
 ESP8266WebServer server(80); 
+
 void handleRoot() 
 {
- String s = MAIN_page; 
- server.send(200, "text/html", s); 
+  String s = MAIN_page; 
+  server.send(200, "text/html", s); 
 }
-void handleLEDon() { 
- Serial.println("LED on page");
- digitalWrite(LED,LOW); 
- server.send(200, "text/html", "LED is ON"); 
+
+void handleLEDon() 
+{ 
+  Serial.println("LED On Request");
+  digitalWrite(LED,LOW); 
+  server.send(200, "text/html", "LED is ON"); 
 }
+
 void handleLEDoff() 
 { 
- Serial.println("LED off page");
- digitalWrite(LED,HIGH); //LED off
- server.send(200, "text/html", "LED is OFF"); 
+  Serial.println("LED off Request");
+  digitalWrite(LED,HIGH);
+  server.send(200, "text/html", "LED is OFF"); 
 }
-void setup(void){
+
+void setup(void)
+{
   Serial.begin(115200);
   WiFi.begin(ssid, password); 
   Serial.println(""); 
@@ -51,9 +57,10 @@ void setup(void){
   WiFi.config(staticIP, subnet, gateway, dns);
   WiFi.begin(ssid, password);
   WiFi.mode(WIFI_STA); 
-  while (WiFi.status() != WL_CONNECTED) {
-  delay(500);
-  Serial.print(".");
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+  `delay(500);
+  `Serial.print(".");
   }
   server.on("/", handleRoot);      
   server.on("/ledOn", handleLEDon); 
@@ -61,6 +68,8 @@ void setup(void){
   server.begin();                 
   Serial.println("WiLamp V1 Server Started");
 }
-void loop(void){
-server.handleClient();         
+
+void loop(void)
+{
+`server.handleClient();         
 }
